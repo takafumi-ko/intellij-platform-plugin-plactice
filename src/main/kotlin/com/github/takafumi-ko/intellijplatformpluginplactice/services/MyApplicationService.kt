b3029b821cdfe2b1bee61d497cae973f1f104d02
GitHub Action <action@github.com>
2020-09-18 18:27:54 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/takafumi-ko/intellijplatformpluginplactice/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.takafumi-ko.intellijplatformpluginplactice.services
+
+import com.github.takafumi-ko.intellijplatformpluginplactice.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
